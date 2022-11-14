689c3cdd142c706bfbc6d7315f4405d7649cc47a
Maripirs <mariapazmaluenda@gmail.com>
2022-11-14 09:36:24 -0800

    commented out computer player

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GA-Project-0
+
+## Tic Tax Toe Game
+
+It's currently a game for 2 players
+Players are 'x', in red. And 'o', in blue.
+Goal of the game is to make 3 in a row of the markers that correspond to the player.
+That can be done in rows, columns or diagonals.
+
+A player should in their turn click the square where they wish to make their next placement.
+If the square in question is empty, their marker will appear there.
+
+If someone wins or the board gets full, the game end results will display and the win tracker will get updated
--- app.js
+++ app.js
@@ -72,8 +72,9 @@ decidePlacement{
             complete
     check if oponent is about to win
             block
-    
-    draw corner
+    look for 'l' in opponent 
+
+
 }
 
 
@@ -95,6 +96,7 @@ const oTrack = document.querySelector('#oTracker')
 const xTrack = document.querySelector('#xTracker')
 const results = document.querySelector('.results')
 let currentPlayer = 'x'
+let otherPlayer = 'o'
 let oWins = 0
 let xWins = 0
 let turnNumber = 1
@@ -103,6 +105,18 @@ let gameGrid = [
     [0, 0, 0],
     [0, 0, 0]
 ]
+
+let winningSets = [
+    [...gameGrid[0]],
+    [...gameGrid[1]],
+    [...gameGrid[2]],
+    [gameGrid[0][0], gameGrid[1][0], gameGrid [2][0]],
+    [gameGrid[0][1], gameGrid[1][1], gameGrid [2][1]],
+    [gameGrid[0][2], gameGrid[1][2], gameGrid [2][2]],
+    [gameGrid[0][0], gameGrid[1][1], gameGrid [2][2]],
+    [gameGrid[0][2], gameGrid[1][1], gameGrid [2][0]]
+]
+
 
 let gameOver = false
 let round = 1
@@ -132,14 +146,15 @@ const clearGame = () => {
 const changeCurrentPlayer = () =>{    
     if (currentPlayer === 'x'){
         currentPlayer = 'o'
+        otherPlayer = 'x'
     } else {
         currentPlayer = 'x'
+        otherPlayer = 'o'
     }
 }
 
 //checks if the current user has 3 in a row or if it's been 9 rounds already
 const checkWinCondition = () => {
-    console.log(round)
     for (let i = 0; i < 3; i++) {
         if (currentPlayer === gameGrid[i][0] &&
             currentPlayer === gameGrid[i][1] &&
@@ -183,7 +198,8 @@ const updateGameGrid =(coordinates) => {
     gameGrid[rvalue][cvalue] = currentPlayer
 }
 
-
+//it will turn the display of the text for game ovet
+//will change text and color depending on the result
 const displayWinner = (winner) => {
     results.style.display = 'flex'
     winText = document.querySelector('.winner')
@@ -201,6 +217,8 @@ const displayWinner = (winner) => {
     }
     gameOver = true
 }
+
+//Updates the player turn and changes color of player marker in text
 const  displayCurrentPlayer = () =>{
     const spanColor = document.querySelector('.playerColor')
     if (gameOver === false){
@@ -217,6 +235,7 @@ const  displayCurrentPlayer = () =>{
 
 }
 
+//depending on current player in will increment that player's win count
 const updateWinTracker =() =>{
     if(currentPlayer === 'x'){
         xWins += 1
@@ -227,11 +246,8 @@ const updateWinTracker =() =>{
     }
 }
 
-
-
+//given the coordenates of the chosen square it will display the current player's marker in that square
 const updateDrawing = (computerChoice) =>{
-    console.log(computerChoice[0], computerChoice[1]);
-    console.log(`#r${computerChoice[0]}c${computerChoice[1]}`);
     const computerSquare = document.querySelector(`#r${computerChoice[0] +1}c${computerChoice[1] + 1}`);
     computerSquare.textContent = currentPlayer
     if (currentPlayer === 'x') {
@@ -241,7 +257,7 @@ const updateDrawing = (computerChoice) =>{
     }
 }
 
-
+//picks an empty square in the game grid. Will loop through the grid picking random rows and columns until one is equal to 0
 const findEmptySpot = () => {
     let computerChoice = null
     while (computerChoice === null) {
@@ -255,22 +271,157 @@ const findEmptySpot = () => {
     }            
 }
 
+//attempt to build an AI
+/*
 
+const almostOverRow =(playerArr, winningRow) =>{
+    for (let i = 0; i < 3; i++){
+        if (playerArr.includes(winningRow[i].join("").toString())){
+            return [i, winningRow[i].indexOf(0)]
+        }
+    }
+}
+const almostOverCol = (playerArr, winningCol) => {
+    for (let i = 0; i < 3; i++){
+        if (playerArr.includes(winningCol[i].join("").toString())){
+            return [winningCol[i].indexOf(0), i]
+        }
+    }
+}
+const almostOverDia = (playerArr, winningDia) => {
+    if (playerArr.includes(winningDia[0].join("").toString())){
+        return [winningDia.indexOf(0), winningDia.indexOf(0)]
+    }
+    if (playerArr.includes(winningDia[1].join("").toString())){
+        if (gameGrid[0][2] === 0){
+            return [0, 2]
+        }else if (gameGrid[1][1] === 0){
+            return [1, 1]
+        }else return [2, 0]
+    }
+} 
+
+
+const checkForAlmostOver = (currentPlayer) =>{
+    console.log('looking if almost over')
+    let cP = currentPlayer
+    let oP = null
+    if (cP === 'x'){
+        oP = 'o'
+    }else {
+       oP ='x'
+    }
+    let almostWinArr = [`${cP}${cP}0`,`0${cP}${cP}`, `${cP}0${cP}`]
+    let almostLostArr = [`${oP}${oP}0`,`0${oP}${oP}`, `${oP}0${oP}`]
+    const winningRow = [
+        [...gameGrid[0]],
+        [...gameGrid[1]],
+        [...gameGrid[2]]
+    ]
+    const winningCol = [
+        [gameGrid[0][0], gameGrid[1][0], gameGrid [2][0]],
+        [gameGrid[0][1], gameGrid[1][1], gameGrid [2][1]],
+        [gameGrid[0][2], gameGrid[1][2], gameGrid [2][2]],
+    ]
+    const winningDia =[
+        [gameGrid[0][0], gameGrid[1][1], gameGrid [2][2]],
+        [gameGrid[0][2], gameGrid[1][1], gameGrid [2][0]]
+    ]
+    if (almostOverRow(almostWinArr, winningRow) != undefined){
+        console.log('almost win row', round)
+        return almostOverRow(almostWinArr, winningRow)
+    }
+    if (almostOverCol(almostWinArr, winningCol) != undefined){
+        console.log('almost win col', round)
+        return almostOverCol(almostWinArr, winningCol)
+    }
+    if (almostOverDia(almostWinArr, winningDia) != undefined){
+        console.log('almost win dia', round)
+        return almostOverDia(almostWinArr, winningDia)
+    }
+    if (almostOverRow(almostLostArr, winningRow) != undefined){
+        console.log('almost lost row', round)
+        return almostOverRow(almostLostArr, winningRow)
+    }
+    if (almostOverCol(almostLostArr, winningCol) != undefined){
+        console.log('almost lost col', round)
+        return almostOverCol(almostLostArr, winningCol)
+    }
+    if (almostOverDia(almostLostArr, winningDia) != undefined){
+        console.log('almost lost dia', round)
+        return almostOverDia(almostLostArr, winningDia)
+    }
+
+
+    return 'nothing'
+}
+
+
+
+
+
+
+const decidePlacement =() =>{
+    if (round === 2 ){
+        if (gameGrid[1][1] === 0 ){
+            return[1,1]
+        }
+        return [0,0]
+    }
+    if (round === 1){
+        return [0,0]
+    }
+    if (round === 3){
+        if (gameGrid[1][2] === 0) return [1,2]
+        if (gameGrid[2][1] === 0) return [2,1]
+        else return [0, 0]
+    }
+    if (round === 4){
+        if (checkForAlmostOver() != 'nothing'){
+            return checkForAlmostOver()
+        }
+        console.log('looking for an L')
+        if (gameGrid[0][0] === otherPlayer && gameGrid[1][2] ===otherPlayer ||
+            gameGrid[0][1] === otherPlayer && gameGrid[2][2] ===otherPlayer ){
+                return [0 ,2]
+            }
+        if (gameGrid[0][2] === otherPlayer && gameGrid[2][1] ===otherPlayer ||
+            gameGrid[1][2] === otherPlayer && gameGrid[2][0] ===otherPlayer ){
+                return [2 ,2]
+            }
+        if (gameGrid[2][2] === otherPlayer && gameGrid[1][0] ===otherPlayer ||
+            gameGrid[2][1] === otherPlayer && gameGrid[0][0] ===otherPlayer ){
+                return [2 ,0]
+            }
+        if (gameGrid[0][2] === otherPlayer && gameGrid[1][0] ===otherPlayer ||
+            gameGrid[0][1] === otherPlayer && gameGrid[1][2] ===otherPlayer ){
+                return [0 ,0]
+            }
+    }
+    if (checkForAlmostOver() != 'nothing'){
+        return checkForAlmostOver()
+    } else return findEmptySpot()
+  
+}
+
+*/
+
+
+//where all the other functions come together (only for computer)
 const computerTurn =(level) =>{
     let computerChoice = null
     if (level === 1){
         computerChoice = findEmptySpot()
-        console.log(computerChoice, 'computer Choice')
-    }else if (level = 2){
+    }else if (level === 2){
         if (computerTurn%2 === 0){
-            findEmptySpot()
+            computerChoice = findEmptySpot()
         } else {
-            decidePlacement()
+            computerChoice = decidePlacement()
         }
-    }else if (level = 2){
-        decidePlacement()
+    }else if (level === 3){
+        computerChoice = decidePlacement()
     }
-
+    console.log(computerChoice,'computer choice' ,round)
     updateGameGrid(computerChoice)
     updateDrawing(computerChoice)
     let winner = checkWinCondition()
@@ -285,8 +436,8 @@ const computerTurn =(level) =>{
 
 
 
-
-
+//where all the other functions come together (player turn)
+//listening for a click in one of the (available) squares
 gameContainer.addEventListener('click', function(e){
     if(gameOver === false){
         if (e.target.textContent === ''){
@@ -306,9 +457,10 @@ gameContainer.addEventListener('click', function(e){
             changeCurrentPlayer()
             displayCurrentPlayer()
             round +=1
-            if (gameOver === false){
-                computerTurn(1)
-            }
+            //uncomment this to play random computer moves
+            // if (gameOver === false){
+            //     computerTurn(1)
+            // }
         } 
     }
 })
@@ -319,3 +471,4 @@ gameContainer.addEventListener('click', function(e){
 
 clearBtn.addEventListener('click', clearGame)
 //checkWinCondition()
+//computerTurn(1)
